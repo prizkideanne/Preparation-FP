2c32ec6513a5dd57731d25465cfdff1afae8be1f
prizkideanne <prizkideanne@gmail.com>
2023-08-07 03:29:48 +0700

    add reusable components

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,25 +1,64 @@
-import axios from "axios";
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+import Button from "./components/buttons/Button";
+import ButtonCircular from "./components/buttons/ButtonCircular";
+import ButtonWithIcon from "./components/buttons/ButtonWithLeadingIcon";
+import PropertyCard from "./components/cards/PropertyCard";
+import TextInput from "./components/forms/TextInput";
+import TableWithSortHeader from "./components/tables/TableWithSortHeader";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {message}
-      </header>
+    <div className="flex flex-col bg-blue-100 m-10 p-5 rounded gap-10">
+      {/* TEXT INPUTS */}
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-bold mb-5">Form Text Input</h1>
+        <div className="gap-4 grid grid-cols-2">
+          <TextInput
+            label={"label"}
+            placeholder={"Text input with label and placeholder"}
+          />
+          <TextInput
+            label={"Password"}
+            type={"password"}
+            placeholder={"Put your password here"}
+          />
+          <TextInput
+            label={"Email"}
+            type={"email"}
+            placeholder={"john@wick.com"}
+          />
+          <TextInput
+            label={"Number"}
+            type={"number"}
+            placeholder={"080989999"}
+          />
+        </div>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-bold mb-5">Buttons</h1>
+        <div className="flex flex-row flex-wrap gap-5">
+          <Button label={"Button with label"} />
+          <ButtonWithIcon label={"Button With Icon"} />
+          <ButtonCircular />
+        </div>
+      </div>
+
+      {/* Property Card */}
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-bold mb-5">Property Card</h1>
+        <div className="flex flex-col flex-wrap gap-5">
+          <PropertyCard />
+        </div>
+      </div>
+
+      {/* TABLES */}
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-bold mb-5">Tables</h1>
+        <div className="gap-4 flex flex-col">
+          <TableWithSortHeader />
+        </div>
+      </div>
     </div>
   );
 }
